27cd28ac0d65ad9b79a0faeac7f225fda6a82bbf
rgmvp <perez4horoneniepwnz@yandex.ru>
2023-01-24 23:24:38 +0300

    done

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,9 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("is");
+        System.out.println("smth");
         //write your code here
     }
 }
